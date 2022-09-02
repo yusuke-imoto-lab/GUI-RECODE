fa308602c5f437ec4febf0105be01989f959c83b
yusukeimoto <85976418+yusukeimoto@users.noreply.github.com>
2022-09-02 10:26:51 +0900

    Update conv.py

--- python/conv.py
+++ python/conv.py
@@ -41,7 +41,7 @@ else:
     print("---- loaded input file", file=sys.stderr)
     recode = screcode.RECODE(seq_target=seq)
     data_scRECODE = recode.fit_transform(np.array(data_pd.values))
-    data_pd = pd.DataFrame(data_scRECODE, index=data_pd.index, colmuns=data_pd.columns)
+    data_pd = pd.DataFrame(np.round(data_scRECODE, decimals=5), index=data_pd.index, colmuns=data_pd.columns)
     print("----  recode done", file=sys.stderr)
     data_pd.to_csv(output_filename,sep=delimiter)
     print("---- wrote output file", file=sys.stderr);
